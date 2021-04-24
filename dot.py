0d914f4ab13404fa637618c81dae41ac5da93f4b
Draco Xu <1628407874@qq.com>
2021-04-24 10:53:05 +0800

    Update dot.py

--- dot.py
+++ dot.py
@@ -41,7 +41,7 @@ mon = monitors.Monitor('hospital6')
 mon.setDistance(57)
 mon.setSizePix([1920, 1080])
 mon.setWidth(52.71)
-myWin = visual.Window([600, 600], units='pixels',monitor=mon)
+myWin = visual.Window([600, 600], units='pixels',monitor=mon, color=(0, 0, 0))
 fixation1 = visual.GratingStim(win=myWin, size=0.5, pos=[0,0], sf=0, rgb=-1)
 
 dotsLeftstop = visual.DotStim(win=myWin, nDots=50, units='pixels', fieldSize=200, fieldShape='circle', coherence=1, dotSize=10, dir=0, speed=0, dotLife=-1)
@@ -83,53 +83,84 @@ def stimulus_random(random_index):
       dotsRandom.draw()
       myWin.flip()
 
+def cart2pol(x, y):
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return(rho, phi)
 
 def fixation():
     fixation1.draw()
     myWin.flip()
     core.wait(1)
 
-def stimulus_chaos():
+direction=1
+def stimulus_chaos(direction):
     nDots = 50
     # The maximum speed of the dots
-    maxSpeed = 10
+    maxSpeed = 10 # 10 is degree, should switch to pixels
     # The size of the field. Note that you also need to modify the `fieldSize`
     # keyword that is passed to `ElementArrayStim` below, due to (apparently) a bug
     # in PsychoPy
     fieldSize = 200
     # The size of the dots
-    dotSize = 10
+    dotSize = 5
     # The number of frames
     nFrames = 1000
-
     # Initial parameters
     dotsTheta = np.random.rand(nDots) * 360
-    dotsRadius = (np.random.rand(nDots) ** 0.5) * 200
-    speed = np.random.rand(nDots) * maxSpeed
-
+    dotsRadius = (np.random.rand(nDots) ** 0.5) * fieldSize
+    dotsX, dotsY = pol2cart(dotsTheta, dotsRadius)
+    speed = maxSpeed
+    if direction == 1: # left
+        directionRange1 = [-180, -20]
+        directionRange2 = [20, 180]
+    elif direction == 2: # right
+        directionRange1 = [-160, 0]
+        directionRange2 = [0, 160]
+ 
     # Create the stimulus array
     dots = visual.ElementArrayStim(myWin, elementTex=None, fieldShape='circle', \
-                                   elementMask='circle', nElements=nDots, sizes=dotSize, units='pix', \
-                                   fieldSize=10000)
+                                   elementMask='circle', nElements=nDots, sizes=dotSize, units='pixels', \
+                                   fieldSize=500)
 
     # Walk through each frame, update the dot positions and draw it
-    for frameN in range(100):
-        # update radius
-          dotsRadius = (dotsRadius + speed)
-        # random radius where radius too large
-          outFieldDots = (dotsRadius >= fieldSize)
-          dotsRadius[outFieldDots] = np.random.rand(sum(outFieldDots)) * fieldSize
-          dotsX, dotsY = pol2cart(dotsTheta, dotsRadius)#坐标转换
-          dots.setXYs(np.array([dotsX, dotsY]).transpose())
-          dots.draw()
-          myWin.flip()
+    dir1 = np.random.rand(int(nDots / 2)) * (directionRange1[1] - directionRange1[0]) + directionRange1[0]
+    dir2 = np.random.rand(int(nDots / 2)) * (directionRange2[1] - directionRange2[0]) + directionRange2[0]
+
+    dir = np.hstack((dir1, dir2))
+    np.random.shuffle(dir)
+
+    for frameN in range(5000):
+        # draw random directions
+      re = frameN % 10
+      while re == 0:
+            dir1 = np.random.rand(int(nDots/2))*(directionRange1[1]-directionRange1[0])+directionRange1[0]
+            dir2 = np.random.rand(int(nDots/2))*(directionRange2[1]-directionRange2[0])+directionRange2[0]
+            dir = np.hstack((dir1, dir2))
+
+      # update position
+      dotsX, dotsY = dotsX + speed*np.cos(dir), dotsY + speed*np.sin(dir)
+      # convert catesian to polar
+      dotsTheta, dotsRadius = cart2pol(dotsX, dotsY)
+      # random radius where radius too large
+
+      outFieldDots = (dotsRadius > fieldSize)
+      dotsRadius[outFieldDots] = np.random.rand(sum(outFieldDots)) * fieldSize
+
+
+      dotsX, dotsY = pol2cart(dotsTheta, dotsRadius)
+      #坐标转换
+      dots.setXYs(np.array([dotsX, dotsY]).transpose())
+      dots.draw()
+      myWin.flip()
+
 
 f = open('numbers2.csv', 'w')
 with f:
     writer = csv.writer(f)
     writer.writerow(['index_trial', 'condition', 'choice','reaction_time'])
 
-for iTrial in range(nTrials):  # loop trials
+"""for iTrial in range(nTrials):  # loop trials
     # draw fixation
 
     fixation()
@@ -164,7 +195,8 @@ for iTrial in range(nTrials):  # loop trials
          writer = csv.writer(f)
          writer.writerow([iTrial,direction_random[iTrial], choice[iTrial], reaction_time])
   # ns.sync()
-  # ns.SendSimpleTimestampedEvent(b'0051')
+  # ns.SendSimpleTimestampedEvent(b'0051')"""
+stimulus_chaos(direction)
 
 myWin.close()
 core.quit()
